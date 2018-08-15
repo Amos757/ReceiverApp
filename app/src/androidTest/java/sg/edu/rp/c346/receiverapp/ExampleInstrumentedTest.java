c4ef6b183bf62c3188e406893b1ef90b9419aa72
AMOS CHUI HON YANG <17045722@rp.edu.sg>
2018-08-15 15:35:56 +0800

    Initial commit

---
+++ app/src/androidTest/java/sg/edu/rp/c346/receiverapp/ExampleInstrumentedTest.java
@@ -0,0 +1,22 @@
+package sg.edu.rp.c346.receiverapp;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("sg.edu.rp.c346.receiverapp", appContext.getPackageName());
+    }
+}
